5d091b86004e7fed4115609cd8bc997c646d5514
serg <serg.neworl@gmail.com>
2018-01-31 19:24:56 +0300

    add fork and sigaction

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+
 
 int main() {
 
-	printf("Hello from C-code :)\n");
+	pid_t pid;
+	
+	if (pid = fork()) {
+		FILE* f = fopen("./pid_proc.txt", "w");
+		fprintf(f, "%d", pid);
+		fclose(f);
+
+		struct sigaction new_action;
+	    new_action.sa_handler = SIG_IGN;
+	    new_action.sa_flags = SA_RESTART;
+
+	    sigemptyset(&new_action.sa_mask);
+
+		sigaddset(&new_action.sa_mask, SIGINT);
+		sigaddset(&new_action.sa_mask, SIGTERM);
+
+		sigaction(SIGINT, &new_action, NULL);
+		sigaction(SIGTERM, &new_action, NULL);
+
+		return 0;
+	}
+
+	// printf("Hello from C-code :)\n");
+
+	return 0;
 }
