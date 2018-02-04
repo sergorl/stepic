b534b334806e6a19a4626859cb0b7a536d3b225b
Sergey <serg.neworl@gmail.com>
2018-02-04 17:01:31 +0300

    Update sem5.c

--- sem5.c
+++ sem5.c
@@ -8,9 +8,13 @@ int main()
     key_t key = ftok("/tmp/sem.temp", 1);
     int sem = semget(key, 16, 0777 | IPC_PRIVATE);
     
-    for(size_t i = 0; i<16; ++i) semctl(sem, i, SETVAL, i);
+    for(size_t i = 0; i<16; ++i) {
+    	union semun arg;
+    	arg.val = i ;
+    	semctl(sem, i, SETVAL, arg);
+    }
     
-    printf("ok!\n");
+    printf("ok.\n");
 
     return 0;
 }
